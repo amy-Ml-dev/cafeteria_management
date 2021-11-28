a8ae35c1497c88f2e1fb816dd128184cc8a19e78
amy-Ml-dev <amita.arora@jcboseust.ac.in>
2021-11-28 19:48:23 +0530

    add and remove column migrations deleted

--- db/migrate/20211125065744_add_price_to_order_items.rb
+++
@@ -1,5 +0,0 @@
-class AddPriceToOrderItems < ActiveRecord::Migration[6.1]
-  def change
-    add_column :order_items, :price, :decimal
-  end
-end
--- db/migrate/20211125070455_add_ordered_at_to_orders.rb
+++
@@ -1,5 +0,0 @@
-class AddOrderedAtToOrders < ActiveRecord::Migration[6.1]
-  def change
-    add_column :orders, :ordered_at, :datetime
-  end
-end
