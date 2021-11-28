19d7510b845b937651d0bacde49dfa472364aa52
amy-Ml-dev <amita.arora@jcboseust.ac.in>
2021-11-28 19:41:31 +0530

    OrderItems table dropped and recreated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_28_133528) do
+ActiveRecord::Schema.define(version: 2021_11_28_140940) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,10 +35,10 @@ ActiveRecord::Schema.define(version: 2021_11_28_133528) do
     t.bigint "menu_item_id"
     t.string "menu_item_name"
     t.string "menu_item_price"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
     t.integer "quantity"
     t.decimal "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "orders", force: :cascade do |t|
